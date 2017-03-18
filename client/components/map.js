74c2dd59a12cd9c3d0ba73b217f39e58f4997e40
Innie Kim <innie.jong.kim@gmail.com>
2017-03-18 14:15:10 -0700

    Refactor to have mapDisplay directive to render map

--- client/components/map.js
+++ client/components/map.js
@@ -1,4 +1,32 @@
 angular.module('etapartments')
-.component('mapDisplay', {
+.directive('mapDisplay', function() {
+  
+  return {
+    scope: {
+      results: '<'
+    },
+    controllerAs: 'map',
+    bindToController: true,
+    controller: 'MapCtrl',
     templateUrl: 'client/htmlTemplates/map.html'
+  }
+})
+
+.controller('MapCtrl', function($scope, $window) {
+
+  var options = {
+    start: {lng: -122.41, lat: 37.78},
+    minZoom: 12,
+    maxZoom: 19,
+    zoom: 13
+  }
+
+  var lastWindow;
+
+  $scope.map = new $window.google.maps.Map(document.getElementById('mapWindow'), {
+    center: options.start,
+    minZoom: options.minZoom,
+    maxZoom: options.maxZoom,
+    zoom: options.zoom
+  })
 })
