918dacf50aa05c41f8e92e0a6660501d9b7b2044
Innie Kim <innie.jong.kim@gmail.com>
2017-03-18 13:18:44 -0700

    Add CSS styling to limit height of resultsList and add scrolling on overflow

--- client/services/requester.js
+++ client/services/requester.js
@@ -3,7 +3,6 @@ angular.module('etapartments')
   this.search = function(callback) {
     $http.get('http://127.0.0.1:8080/yelp')
       .then(function(data) {
-          console.log(data.data.businesses);
         if (callback) {
           callback(data.data.businesses);
         }
