d8628209621d7155e637a570940227d1ae56dd5c
Matt Schmitz <mattschmitz9@gmail.com>
2017-03-20 15:48:31 -0700

    Outline rankLocations function

---
+++ server/utils/rankLocations.js
@@ -0,0 +1,17 @@
+/*
+
+
+INPUT: array of yelp bizes, array of anchors
+
+  get travel times for each anchor to each busines - store in array
+    calls to distance matrix api
+
+  for each business: 
+    -get average travel time for each biz
+    -get std for each biz
+    -generate rank for each biz
+    -create object 'travelTimes' with the above, add this object to array of yelp bizes
+
+OUTPUT: array of yelp bizes
+
+*/
