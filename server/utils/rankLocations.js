22df03253bebada5bcb250339896ae7da0730cb4
Matt Schmitz <mattschmitz9@gmail.com>
2017-03-21 11:51:50 -0700

    get travel times for each business in rankLocations

--- server/utils/rankLocations.js
+++ server/utils/rankLocations.js
@@ -1,38 +1,64 @@
-/*
-
-INPUT: array of yelp bizes, array of anchors
-
-  get travel times for each anchor to each busines - store in array
-    calls to distance matrix api
-
-  for each business: 
-    -get average travel time for each biz
-    -get std for each biz
-    -generate rank for each biz
-    -create object 'travelTimes' with the above, add this object to array of yelp bizes
-
-OUTPUT: array of yelp bizes
-
-*/
-
 var _ = require('underscore');
 var gHelpers = require('./gHelpers')
 
-module.exports = function(yelpData, anchors, cb) {
+
+module.exports = function(yelpData, anchors, travelParams, cb) {
+// INPUT: array of yelp bizes, array of anchors
 
   var bizes = JSON.parse(yelpData).businesses
+  // console.log(bizes[0].coordinates)
 
-  // console.log(bizes[0].coordinates);
-  //build strings that can be passed into matrix Call
-      // {
-      //     origins: '37.783617,-122.408955', 
-      //     destinations: '37.770841,-122.423786|37.777179,-122.425929',
-      //     mode: 'driving'
-      //   }
 
-  //take averages, std, and rank - add to help data
+//   get travel times for each anchor to each busines - store in array
+//     calls to distance matrix api
+  // var originCoordinates = '';
+  //get origins
 
-  //sort by rank
+  var anchorCoords = _.map(anchors, function(anchor){
+    return (anchor.coordinates.lat + ',' + anchor.coordinates.lng);
+  })
 
-  cb(yelpData);
+  var bizCoordinatesString = _.map(bizes, function(biz){
+    return (biz.coordinates.latitude + ',' + biz.coordinates.longitude);
+  }).join('|')
+  
+  var matrix = [];
+  var resultsCount = 0;
+
+  _.each(anchorCoords, function(anchorCoordPair, index) {
+    var params = {
+      origins: anchorCoordPair,
+      destinations: bizCoordinatesString,
+      mode: travelParams.travel_mode
+    };
+
+    gHelpers.getTravelTimes(params, function(results){
+      matrix[index] = results;
+      resultsCount++;
+
+      //once all the results have come in
+      if (resultsCount === anchorCoords.length){
+        console.log(matrix);
+        cb(yelpData);
+
+      //   //   for each business: 
+      //   //     -get average travel time for each biz
+      //   //     -get std for each biz
+      //   //     -generate rank for each biz
+      //   //     -create object 'travelTimes' with the above, add this object to array of yelp bizes
+      //   _.each(bizes, function(biz, index) {
+      //     travelTimes = {};
+      //     travelTimes.avg = average(matrix,i)
+
+      //   });
+
+      }
+    })
+  })
+
+
+
+
+
+
 };
