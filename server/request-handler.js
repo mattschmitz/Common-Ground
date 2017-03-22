e3075004a16b9800e2cb0a6c7a53da13602e2d3d
Matt Schmitz <mattschmitz9@gmail.com>
2017-03-21 17:08:32 -0700

    refactor ranking to work with updated parameter names

--- server/request-handler.js
+++ server/request-handler.js
@@ -4,12 +4,7 @@ var rankLocations = require('./utils/rankLocations');
 //add body parser...
 
 exports.getResults = function(req, res){
-
-  // //currently, anchor:
-  // [ { name: 'Matthew S Schmitz',
-  //   address: '824 East Forest Hills Blvd, Durham, NC 27707' } ] [ [ 35.982934, -78.9101589 ] ]
-
-  // //would like req.body to be of form:
+  // req.body should be of form:
   // { 
   //   anchors: [
   //     {
@@ -34,25 +29,9 @@ exports.getResults = function(req, res){
   //     travel_time: 10 
   //   } 
   // }
-
-  //dummy data
-  var anchors = [
-      {
-        name: 'Hack Reactor', 
-        address: '944 Market St, San Francisco, CA 94102', 
-        coordinates: {lat: '37.783617', lng: '-122.408955'}
-      }, 
-      {
-        name: 'Home', 
-        address: '33 Pearl St, San Francisco, CA 94103', 
-        coordinates: {lat: '37.770841', lng: '-122.423786'}
-      },
-    ]
-
-  // console.log('request-handler.js req.body:', req.body);
-
-  yelp.getBusinesses(req.body.yelp, function(yelpResults) {
-    rankLocations(yelpResults, anchors, req.body.google, function(rankedResults){
+  
+  yelp.getBusinesses(req.body.yelpParams, function(yelpResults) {
+    rankLocations(yelpResults, req.body.anchors, req.body.travelParams, function(rankedResults){
       res.send(rankedResults);
     });
   });
