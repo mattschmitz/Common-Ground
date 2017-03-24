9aec836178c31c75792269d9399766e493b628d2
Innie Kim <innie.jong.kim@gmail.com>
2017-03-23 23:58:43 -0700

    Implement Haversine formula to calculate true distance between points
    Refactor setSearchArea and getMaximumDist
    - setSearchArea now ensures we have a minimum radius of 1000m and a maximum radius of 40000m
    - getMaximumDist turns the km into m and the diameter into a radius, returns the floor afterward to ensure an integer is returned

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,12 +1,12 @@
 var getMaximumDist = function (coords) {
   var matrix = [];
   for (var i = 0 ; i<coords.length; i++) {
-    for (var j = 0; j<coords.length; j++) { 
-      matrix.push(Math.sqrt(Math.pow((coords[i][0] - coords[j][0]), 2) +
-                         Math.pow((coords[i][1] - coords[j][1]), 2)));
+    for (var j = 1; j<coords.length; j++) { 
+      matrix.push(haversine(coords[i][0], coords[i][1], coords[j][0], coords[j][1]));
     }
   }
-  return Math.ceil(max(matrix));
+  var maxDist = max(matrix) * 500;
+  return Math.floor(maxDist);
 }
 
 var max = function(array) {
@@ -58,6 +58,22 @@ var secondsToString = function(seconds) {
     return formatPlural(seconds, ' sec', ' sec');
   }
 };
+
+/* 
+Haversine formula implementation from https://rosettacode.org/wiki/Haversine_formula#JavaScript
+Takes in 4 arguments, (latitude1, longitude1, latitude2, longitude2) and returns km
+*/
+
+var haversine = function() {
+       var radians = Array.prototype.map.call(arguments, function(deg) { return deg/180.0 * Math.PI; });
+       var lat1 = radians[0], lon1 = radians[1], lat2 = radians[2], lon2 = radians[3];
+       var R = 6372.8; // km
+       var dLat = lat2 - lat1;
+       var dLon = lon2 - lon1;
+       var a = Math.sin(dLat / 2) * Math.sin(dLat /2) + Math.sin(dLon / 2) * Math.sin(dLon /2) * Math.cos(lat1) * Math.cos(lat2);
+       var c = 2 * Math.asin(Math.sqrt(a));
+       return R * c;
+}
 
 module.exports.getMaximumDist = getMaximumDist;
 module.exports.findCentroid = findCentroid;
